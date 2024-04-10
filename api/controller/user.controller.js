e1220dad75eb669ea01dfdf587060ca6ccb42b5b
Rahul Choudhary <notthefakerahul@gmail.com>
2024-04-10 22:52:45 +0530

    created a test api route

---
+++ api/controller/user.controller.js
@@ -0,0 +1,6 @@
+export const getUser = (req, res)=>{
+    res.json({
+        message: "API is working"
+    })
+}
+
---
+++ api/routes/user.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getUser } from '../controller/user.controller.js';
+
+const router = express.Router();
+
+router.get('/test', getUser)
+
+
+export default router;
