1f220b8cec7904b1aeb3d1b73c2acb16be1c58f3
Rahul Choudhary <notthefakerahul@gmail.com>
2024-04-11 23:27:33 +0530

    create signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model.js'
 import asyncHandler from 'express-async-handler'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = asyncHandler(async (req, res, next) => {
   const { username, email, password } = req.body
@@ -14,14 +15,43 @@ export const signup = asyncHandler(async (req, res, next) => {
     email === '' ||
     password === ''
   ) {
-
-    next(errorHandler(400, "All fields are required!"));
-    
+    next(errorHandler(400, 'All fields are required!'))
   }
   const hashPassword = bcryptjs.hashSync(password, 10)
-  const newUser = new User({ username, email, password:hashPassword })
+  const newUser = new User({ username, email, password: hashPassword })
 
-  await newUser.save();
+  await newUser.save()
 
-  res.json("Signup Successful")
+  res.json('Signup Successful')
+})
+
+export const signin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body
+
+  if (!email || !password || email === null || password === null) {
+    next(errorHandler(400, 'All fields are required'))
+  }
+
+  const validUser = await User.findOne({ email })
+  if (!validUser) {
+    return next(errorHandler(404, 'Email or Password not correct'))
+  }
+
+  const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+  if (!validPassword) {
+    return next(errorHandler(400, 'Email or Password not correct'))
+  }
+  //separating password from the rest of the validUser object
+  const {password: pass, ...rest} = validUser._doc;
+  const token = jwt.sign(
+    {
+      id: validUser._id,
+    },
+    process.env.JWT_SECRET,
+  )
+
+  res.status(200).cookie('accessToken', token, {
+    httpOnly: true,
+  }).json(rest);
 })
