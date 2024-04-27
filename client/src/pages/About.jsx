ad6b42a02b78082812098749d8e42bdf01445102
Rahul Kumar <rahulchoudhary0775@gmail.com>
2024-04-27 15:03:51 +0530

    Completed about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,36 @@
-
 function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className='max-w-2xl mx-auto p-3 text-center'>
+                <div className=''>
+                    <h1 className="text-3xl font-semibold text-center my-7">About Rahul's Blog</h1>
+                    <div className='text-md text-gray-500 flex flex-col gap-6'>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Aspernatur cupiditate neque repellat officia
+                            adipisci voluptatum veritatis quod quasi, vero
+                            architecto maiores magnam, natus quos at quibusdam.
+                            Nihil quae dolores similique!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Deleniti temporibus aliquid cumque officiis
+                            enim quia illum animi doloremque sunt, dolore
+                            voluptate ipsa, est, consequatur nihil optio ad!
+                            Voluptates, at minima?
+                        </p>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Rem voluptates obcaecati in, libero repellat,
+                            cumque tenetur, aspernatur iusto architecto facilis
+                            ex quibusdam quos vitae. Ratione, id ipsum! Quis,
+                            architecto quo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default About
